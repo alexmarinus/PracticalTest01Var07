16364dd25346e4cc084a27947baf32873aedfe0f
alexmarinus <alex.marinus94@gmail.com>
2017-04-04 09:10:31 +0300

    Taskuri colocviu 1

--- PracticalTest01Var07/app/src/main/java/practicaltest01var07/eim/systems/cs/pub/ro/practicaltest01var07/PracticalTest01Var07MainActivity.java
+++ PracticalTest01Var07/app/src/main/java/practicaltest01var07/eim/systems/cs/pub/ro/practicaltest01var07/PracticalTest01Var07MainActivity.java
@@ -2,12 +2,58 @@ package practicaltest01var07.eim.systems.cs.pub.ro.practicaltest01var07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01Var07MainActivity extends AppCompatActivity {
+
+    private EditText nextTermText = null, allTermsText = null;
+    private Button addButton = null, computeButton = null;
+    private String expression = "";
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                //Pentru caseta de text, golesc textul
+                case R.id.editText:
+                    nextTermText.setText("");
+                    break;
+                case R.id.editText2:
+
+                //Pentru butonul de Add, preiau next term
+                case R.id.button:
+                    String nextTerm = nextTermText.getText().toString();
+                    if (!nextTerm.isEmpty()) {
+                        if (expression.isEmpty())
+                            expression = nextTerm;
+                        else
+                            expression = expression + "+" + nextTerm;
+                    }
+                    allTermsText.setText (expression);
+                    break;
+                //Pentru butonul de compute, calculez expresia
+                case R.id.button2:
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var07_main);
+
+        nextTermText = (EditText) findViewById(R.id.editText);
+        allTermsText = (EditText) findViewById(R.id.editText2);
+
+        addButton = (Button) findViewById(R.id.button);
+        computeButton = (Button) findViewById(R.id.button2);
+
+        addButton.setOnClickListener(buttonClickListener);
+        computeButton.setOnClickListener(buttonClickListener);
+        nextTermText.setOnClickListener(buttonClickListener);
     }
 }
---
+++ PracticalTest01Var07/app/src/main/java/practicaltest01var07/eim/systems/cs/pub/ro/practicaltest01var07/PracticalTest01Var07SecondaryActivity.java
@@ -0,0 +1,34 @@
+package practicaltest01var07.eim.systems.cs.pub.ro.practicaltest01var07;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+
+public class PracticalTest01Var07SecondaryActivity extends AppCompatActivity {
+
+    private String expressionToCompute = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var07_main);
+
+        Intent intent = getIntent();
+
+        if (intent != null && intent.getExtras().containsKey("expression")) {
+            expressionToCompute = intent.getStringExtra("expression");
+            expressionToCompute = expressionToCompute.replaceAll("\\+", " +");
+            int expressionresult = 0;
+            String[] expressionTokens = expressionToCompute.split(" +");
+            for (int i = 0; i < expressionTokens.length; i ++) {
+                expressionresult += Integer.parseInt(expressionTokens[i]);
+            }
+        }
+
+        Intent computeIntent = new Intent();
+        
+
+    }
+}
